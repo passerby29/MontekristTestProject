0ae6fc456b4ecd24951dd3d621333cc021a400d9
passerby <yusuf.muratov29@gmail.com>
2023-08-07 15:55:23 +0300

    adding recycler view to fragment

--- app/src/main/java/dev/passerby/montekristtestproject/presentation/MainActivity.kt
+++ app/src/main/java/dev/passerby/montekristtestproject/presentation/MainActivity.kt
@@ -1,12 +1,16 @@
 package dev.passerby.montekristtestproject.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dev.passerby.montekristtestproject.R
+import dev.passerby.montekristtestproject.presentation.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.mainContainer, MainFragment())
+            .commit()
     }
 }
--- app/src/main/java/dev/passerby/montekristtestproject/presentation/fragments/MainFragment.kt
+++ app/src/main/java/dev/passerby/montekristtestproject/presentation/fragments/MainFragment.kt
@@ -1,21 +1,47 @@
 package dev.passerby.montekristtestproject.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.passerby.montekristtestproject.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import dev.passerby.montekristtestproject.databinding.FragmentMainBinding
+import dev.passerby.montekristtestproject.presentation.adapters.PersonInfoAdapter
 import dev.passerby.montekristtestproject.presentation.viewmodels.MainViewModel
 
 class MainFragment : Fragment() {
 
-    private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding
+        get() = _binding ?: throw RuntimeException("FragmentMainBinding is null")
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val personInfoAdapter = PersonInfoAdapter(requireContext())
+        binding.personRecyclerView.apply {
+            adapter = personInfoAdapter
+            itemAnimator = null
+        }
+        viewModel.personListInfo.observe(viewLifecycleOwner) {
+            personInfoAdapter.submitList(it)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
